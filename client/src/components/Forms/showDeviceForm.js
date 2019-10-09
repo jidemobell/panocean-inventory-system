e38963967889196920bb9251b3fa85f496b53579
jidemobell <jidd32@gmail.com>
2019-10-09 12:32:43 +0100

    update show device form

--- client/src/components/Forms/showDeviceForm.js
+++ client/src/components/Forms/showDeviceForm.js
@@ -1,124 +1,84 @@
-import React, {  useReducer } from "react";
+import React, { useReducer } from "react";
 // import axios from 'axios'
 
-
-
-
-
-function  ShowDeviceForm() {
-
-
+function ShowDeviceForm() {
   return (
     <div className="container add-user-form">
       <div className="row">
-        <div className="col-md-8 order-md-1">
-          <h4 className="mb-3">Add a new user | Assign to Device </h4>
-          <hr className="mb-4" />
-          <form
-            className="needs-validation"
-            novalidate
-          >
+        <div className="col">
+          {/* <h4 className="mb-3"> Find and List Devices </h4> */}
+          {/* <hr className="mb-4" /> */}
+          <form className="needs-validation" novalidate>
             <div className="row">
-              <div className="col-md-6 mb-3">
-                <label for="firstName">First name</label>
+              <div className="col-md-3 mb-3">
+                <label for="state">Options</label>
+                <select
+                  className="custom-select d-block w-100"
+                  id="type"
+                  required
+                >
+                  <option value="">Choose...</option>
+                  <option>Description</option>
+                  <option>Department</option>
+                  <option>Floor</option>
+                  <option>User</option>
+                </select>
+                <div className="invalid-feedback">
+                  Please provide a valid Type.
+                </div>
+              </div>
+              <div className="col-md-3 mb-3">
+                <label for="firstName">{"Description"}</label>
                 <input
                   type="text"
                   className="form-control"
                   id="firstName"
                   placeholder=""
-                  defaultValue={''}
+                  defaultValue={""}
                   required
                 />
                 <div className="invalid-feedback">
                   Valid first name is required.
                 </div>
               </div>
-              <div className="col-md-6 mb-3">
-                <label for="lastName">Last name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="lastName"
-                  placeholder=""
-                  defaultValue={''}
-                  required
-                />
-                <div className="invalid-feedback">
-                  Valid last name is required.
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4 mb-3">
-              <label for="state">Type</label>
-              <select
-                className="custom-select d-block w-100"
-                id="type"
-                required
-              >
-                <option value="">Choose...</option>
-                <option>Intern</option>
-                <option>Visitor</option>
-                <option>Staff</option>
-              </select>
-              <div className="invalid-feedback">
-                Please provide a valid Type.
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-5 mb-3">
-                <label for="country">Location</label>
-                <select
-                  className="custom-select d-block w-100"
-                  id="location"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  <option value="hq">HQ</option>
-                  <option value="operations base">Operations Base</option>
-                  <option value="gas plant">Gas Plant</option>
-                  <option value="agbor">PSC Flow Station</option>
-                  <option value="nepl-hq">NEPL HQ</option>
-                  <option value="awoba">NEPL AWOBA</option>
-                  <option value="ekulama">NEPL EKULAMA</option>
-                  <option value="portharcourt">NEPL PH</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please select a valid location.
-                </div>
-              </div>
-              <div className="col-md-4 mb-3">
-                <label for="state">Floor</label>
-                <select
-                  className="custom-select d-block w-100"
-                  id="floor"
-                  required
-                >
-                  <option value="">Choose...</option>
-                  {/* {floorArray.map((val, i) => {
-                    return <option value={String(i + 1)} key={`select-${i}`}>{val}</option>;
-                  })} */}
-                </select>
-                <div className="invalid-feedback">
-                  Please provide a valid Floor.
-                </div>
-              </div>
               <div className="col-md-3 mb-3">
-                <label for="staff-number">ID</label>
+                <label for="state">Options</label>
+                <select
+                  className="custom-select d-block w-100"
+                  id="type"
+                  required
+                >
+                  <option value="">Choose...</option>
+                  <option>None</option>
+                  <option>Contains</option>
+                  <option>is exactly</option>
+                  <option>Starts with</option>
+                </select>
+                <div className="invalid-feedback">
+                  Please provide a valid Type.
+                </div>
+              </div>
+							<div className="col-md-3 mb-3">
+                <label for="firstName">{"Description"}</label>
                 <input
                   type="text"
                   className="form-control"
-                  id="staff-number"
-                  placeholder="STAFF-ID"
-                  defaultValue={''}
+                  id="firstName"
+                  placeholder=""
+                  defaultValue={""}
                   required
                 />
-                <div className="invalid-feedback">ID required.</div>
+                <div className="invalid-feedback">
+                  Valid first name is required.
+                </div>
               </div>
             </div>
-            <hr className="mb-4" />
-            <button className="btn btn-secondary" type="submit">
-              Add User
-            </button>
+						<hr />
+						<div>
+                <button className="btn btn-success" type="submit">
+                  Add User
+                </button>
+              </div>
           </form>
         </div>
       </div>
